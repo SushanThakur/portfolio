cc7d003c186585d9914a599ad0e914ee992755fb
SushanThakur <mail@sushant.name.np>
2024-10-21 12:56:18 +0545

    Responsive Home Page

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,16 +1,26 @@
 const menu = document.getElementById('menu')
 const close = document.getElementById('close')
 const nav = document.getElementById('home-mob-nav-ul')
+const exit = document.getElementById('mobile-nav-exit')
 
 
 menu.addEventListener('click', () => {
     menu.style.display = 'none';
     close.style.display = 'flex';
     nav.style.display = 'flex';
+    exit.style.display = 'flex';
 })
 
 close.addEventListener('click', () => {
     menu.style.display = 'flex';
     close.style.display = 'none';
     nav.style.display = 'none';
+    exit.style.display = 'none';
+})
+
+exit.addEventListener('click', () => {
+    menu.style.display = 'flex';
+    close.style.display = 'none';
+    nav.style.display = 'none';
+    exit.style.display = 'none';
 })
